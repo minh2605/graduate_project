9df38475d60d573dc13eb49689be09178d681b4c
Minh Tran <bluewings2605@gmail.com>
2022-07-15 23:05:59 +0700

    update fe

--- fe_store/src/api/axios.ts
+++ fe_store/src/api/axios.ts
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 const PORT = process.env.REACT_APP_PORT;
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const axiosInstance = axios.create({
-  baseURL: `${BASE_URL}:${PORT}/api/`,
+  baseURL: `${BASE_URL}/api/`,
 });
 
 axiosInstance.interceptors.request.use(async (config) => {
